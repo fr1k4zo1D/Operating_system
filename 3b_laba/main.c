8c99b5865b08b72f2a671ef279add4704377e445
fr1k4zo1D <kamalov.a1b3@yandex.ru>
2019-12-25 18:51:28 +0300

    3_b_laba

--- 3b_laba/main.c
+++ 3b_laba/main.c
@@ -6,52 +6,63 @@
 #include <time.h>
 #include <unistd.h>
 
+#define FOR_ALL 0777
+#define size_of_time 25
+#define size_of_pid 5
+#define FIFO_file "FIFO" 
 
 main()
 {
-   unlink("FIFO"); 
-   int fifo = mkfifo ("FIFO", 0777);
-   if(fifo < 0)
-	printf("Fifo cannot be created \n");
-   pid_t pid;
-   switch(pid = fork())
-   {
-	case 0:
-        {
-	    int fifo_read = open("FIFO", O_RDONLY);
-	    if (fifo_read == -1)
-	    	printf("Фифио не может открыть файл на чтение \n");
-	    char *buf = (char*) calloc (25, sizeof(char));
-	    read(fifo_read, buf, 5);
-	    printf("CHILD: Пид бати = %s\n", buf);
-	    read(fifo_read, buf, 25);
-	    printf("CHILD: Время у бати = %s\n", buf); 
-	    close(fifo_read);
-	    time_t child_time = time(0);
-	    printf("CHILD: Мое время = %s\n", ctime(&child_time));
-	    
-	    break;
-	}
-
-	default:
+	unlink(FIFO_file); 
+	int fifo = mkfifo (FIFO_file, FOR_ALL);
+	if(fifo < 0)
 	{
-	    
-	    int fifo_write = open("FIFO", O_WRONLY);
-	    if(fifo_write == -1)
-		printf("Фифо не может открыть файл на запись \n");
-	    time_t parent_time = time(0);
-	    pid_t  parent_pid = getpid();
-	    char buff[25];
-	    sprintf(buff, "%d", parent_pid);
-		printf("PARENT: Мое время = %s\n",ctime(&parent_time));
-	    printf("PARENT: Мой пид = %d\n", parent_pid);
-	    write(fifo_write, &buff, strlen(buff));
-	    write(fifo_write, ctime(&parent_time), 50);
-	    close(fifo_write);
-	    break;
+		printf("Fifo cannot be created \n");
+		return 0;
+	}
+	pid_t pid;
+	switch(pid = fork())
+	{
+		case 0:
+        {
+	    	int fifo_read = open(FIFO_file, O_RDONLY);
+	    	if (fifo_read == -1)
+			{
+	    		printf("Фифио не может открыть файл на чтение \n");
+	    		break;
+			}
+			char *buf = (char*) calloc (size_of_time, sizeof(char));
+	    	read(fifo_read, buf, size_of_pid);
+	    	printf("CHILD: Пид бати = %s\n", buf);
+	    	read(fifo_read, buf, size_of_time);
+	    	printf("CHILD: Время у бати = %s\n", buf); 
+	    	close(fifo_read);
+	    	time_t child_time = time(NULL);
+	    	printf("CHILD: Мое время = %s\n", ctime(&child_time));	    
+	    	break;
+		}
+
+		default:
+		{
+	    	int fifo_write = open(FIFO_file, O_WRONLY);
+	    	if(fifo_write == -1)
+			{
+				printf("Фифо не может открыть файл на запись \n");
+	    		break;
+			}
+			time_t parent_time = time(NULL);
+	    	pid_t  parent_pid = getpid();
+	    	char buff[size_of_time];
+	    	sprintf(buff, "%d", parent_pid);
+			printf("PARENT: Мое время = %s\n",ctime(&parent_time));
+	    	printf("PARENT: Мой пид = %d\n", parent_pid);
+	    	write(fifo_write, &buff, strlen(buff));
+	    	write(fifo_write, ctime(&parent_time), size_of_time*2);
+	    	close(fifo_write);
+	    	break;
 	    
        } 
-    } 
+	} 
    
-    return 0;
+	return 0;
 }
