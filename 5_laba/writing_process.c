7d2dbfa7192c3ef7142aba24414bd847510d67ab
fr1k4zo1D <kamalov.a1b3@yandex.ru>
2019-12-27 13:58:34 +0300

    add 5 laba

---
+++ 5_laba/writing_process.c
@@ -0,0 +1,67 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <errno.h>
+#include <semaphore.h>
+#include <sys/sem.h>
+
+#define shmem_file "shared_memory"
+
+struct sembuf sem_lock = {0, -1, 0};
+struct sembuf sem_unlock = {0, 1, 0};
+
+int main()
+{
+	key_t key = ftok(shmem_file, 65);
+	if(key == -1)
+	{
+		printf("bad key errno:%d", errno);
+		return 0;
+	}
+
+	int shmid = shmget(key, 50, IPC_CREAT|0666);
+	if(shmid == -1)
+	{
+		perror("Shared memoty cannot be created \n");
+		return 0;
+	}
+
+	int semaphore = semget(4032, 1, IPC_CREAT|0666);
+	if(semaphore == -1)
+	{
+		perror("Semaphore cannot be created \n");
+		return 0;
+	}
+
+	char *addr = shmat(shmid, NULL, 0);
+	if (addr == (char*)-1)
+	{
+		perror("Shmat error \n");
+		return 0;
+	}
+
+	printf("Writing process: %d, %d\n", key, shmid);
+	
+	semop(semaphore, &sem_unlock, 1);
+	while(1)
+	{
+		semop(semaphore, &sem_lock,1);
+		time_t time_ = time(0);
+		printf("Writing process: Time -- %s", ctime(&time_));
+		sprintf(addr, "%s", ctime(&time_));
+		sleep(1);
+		semop(semaphore, &sem_unlock,1);
+
+	}
+	
+	shmdt(shmem_file);
+	shmctl(shmid, IPC_RMID, 0);
+	//sem_destroy(&semaphore);
+	return 0;
+}
